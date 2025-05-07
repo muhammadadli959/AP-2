fb87843817dce640955b14a35f3a28d36e7fc5e0
muhammadadli959 <muhammadadlii5643@gmail.com>
2025-05-07 14:25:42 +0700

    Delete headerCustom.h

--- headerCustom.h
+++
@@ -1,8 +0,0 @@
-#define l << 
-#define u using namespace std;
-#define output cout
-#define nilai int
-#define kelipatan double
-#define BarisBaru endl
-#define perangkat system
-#define kembalikan return
