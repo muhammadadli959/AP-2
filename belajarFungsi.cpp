21a8766629eef22e323d85aed3376ae4d2365dc3
muhammadadli959 <muhammadadlii5643@gmail.com>
2025-05-07 14:19:54 +0700

    pertemuan 6

---
+++ belajarFungsi.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+void sapa(string nama) {
+    cout << "halo" << nama <<" selamat datang di AP 2 !" << endl;
+
+}
+int main(){
+    string namapengguna = " adli";
+    sapa(namapengguna);
+    return  0;
+}
